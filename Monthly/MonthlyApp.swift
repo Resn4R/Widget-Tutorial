57513e410591c51f4a056d32d815170db56adf80
Resn4R <purview-joust.0x@icloud.com>
2024-01-25 16:28:36 +0000

    Initial Commit

---
+++ Monthly/MonthlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MonthlyApp.swift
+//  Monthly
+//
+//  Created by Vito Borghi on 25/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct MonthlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
