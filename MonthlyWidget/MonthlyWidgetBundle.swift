460ebd51cfd60186881d71d71b3429a7a691d6ba
Resn4R <purview-joust.0x@icloud.com>
2024-01-25 16:31:49 +0000

    first commit

---
+++ MonthlyWidget/MonthlyWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  MonthlyWidgetBundle.swift
+//  MonthlyWidget
+//
+//  Created by Vito Borghi on 25/01/2024.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct MonthlyWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        MonthlyWidget()
+    }
+}
